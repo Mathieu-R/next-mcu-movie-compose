5290e90473e4efe7ac5da06dd0e2a914d1107d2b
Mathieu-R <mathieu0709@gmail.com>
2022-09-03 08:45:50 +0200

    fix unresolved reference for hiltViewModel()

--- app/src/main/java/com/mathieu/next_mcu/MainActivity.kt
+++ app/src/main/java/com/mathieu/next_mcu/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.mathieu.next_mcu.ui.theme.NextmcuTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,7 +36,7 @@ class MainActivity : ComponentActivity() {
 fun Home(
   viewModel: MainViewModel = hiltViewModel()
 ) {
-
+  
 }
 
 @Preview(showBackground = true)
